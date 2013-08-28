39eccba38021a346a44cdd2663b8982cc4f5511f
alfrescian <jan@alfrescian.com>
2013-08-28 13:19:22 +0200

    init admin service impl (local & static data)

--- app/scripts/services/administrationService.js
+++ app/scripts/services/administrationService.js
@@ -1,6 +1,83 @@
 'use strict';
 
-angular.module('test1App')
+angular.module('fireTimeTracker')
   .service('administrationService', function administrationService() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+      
+      
+      this.getCustomers = function (userId){
+        return this.customers;
+      };
+      
+      this.getProjects = function (customerId){
+        return this.projects;
+      };
+      this.getTasks = function (projectId){
+        return this.tasks;
+      };
+      
+      
+      
+      //test objects
+      this.customers = [
+            {
+                name : 'Customer 1',
+                status: 'active',
+                id: 'cust_1'
+            },
+            {
+                name : 'Customer 2',
+                status: 'active',
+                id: 'cust_2'
+            },
+            {
+                name : 'Customer 3',
+                status: 'inactive',
+                id: 'cust_3'
+            }
+        ];
+      this.tasks = [
+            {
+                name : 'Task 1',
+                plannedEffort: 2,
+                status: 'active',
+                id: 'task_1'
+            },
+            {
+                name : 'Task 1',
+                plannedEffort: 2,
+                status: 'active',
+                id: 'task_1'
+            },
+            {
+                name : 'Task 1',
+                plannedEffort: 2,
+                status: 'active',
+                id: 'task_1'
+            }
+        ];
+      this.projects = [
+            {
+                name : 'Project 1',
+                number: 100123,
+                plannedEffort: 48,
+                status: 'active',
+                id: 'prj_1'
+            },
+            {
+                name : 'Project 2',
+                number: 100124,
+                plannedEffort: 8,
+                status: 'inactive',
+                id: 'prj_2'
+            },
+            {
+                name : 'Project 3',
+                number: 100124,
+                plannedEffort: 12,
+                status: 'active',
+                id: 'prj_3'
+            }
+        ];
+    
+          
   });
