a00f2c79b795f9a1d7ee91d4056824c5db773cc0
Florian Maul <florian.maul@gmail.com>
2013-08-28 11:21:53 +0200

    Added admin service

---
+++ app/scripts/services/administrationService.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('test1App')
+  .service('administrationService', function administrationService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
