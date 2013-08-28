3cb7030e48a1717b33afcfe7e41548173ab32e5f
Florian Maul <florian.maul@gmail.com>
2013-08-28 17:13:54 +0200

    Implemented booking service call

--- app/scripts/services/historyService.js
+++ app/scripts/services/historyService.js
@@ -3,65 +3,62 @@
 angular.module('fireTimeTracker')
 .filter('dynamicDuration', function() {
     return function(input, scope) {
+      if (input) {
         if (input.asSeconds() < 60) {
           return  input.format("s") + "s";
         }
-        else return  input.format("H:mm");
+        else return input.format("H") + "h" + input.format("mm") + "m";
+      }
     }
 })
-  .service('historyService', function historyService() {
-
-  	this.entries = [
-  		{
-  			day : 1377686946998,
-  			tasks : [{
-  				name : "Task1",
-  				start: 1377686941998, // "12:00:05"
-  				end: 1377686946998,  // 12:14:14
-  				comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, non, assumenda, dolor, corrupti dolore quibusdam velit sed voluptas quasi laboriosam voluptatem excepturi iusto accusantium ipsum autem impedit consequuntur voluptatibus eligendi!"
-  			},
-  			{
-  				name : "Task2",
-  				start: 1377686940998,
-  				end: 1377686946998, 
-  				comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, non, assumenda, dolor, corrupti dolore quibusdam velit sed voluptas quasi laboriosam voluptatem excepturi iusto accusantium ipsum autem impedit consequuntur voluptatibus eligendi!"
-  			},
-  			{
-  				name : "Task3",
-  				start: 1377686916998,
-  				end: 1377687916998, 
-  				comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, non, assumenda, dolor, corrupti dolore quibusdam velit sed voluptas quasi laboriosam voluptatem excepturi iusto accusantium ipsum autem impedit consequuntur voluptatibus eligendi!"
-  			}]
-  		},
-  		{
-  			day : 234234324,
-  			tasks : [{
-  				name : "Task1",
-  				start: 23847238947,
-  				end: 2348723432, 
-  				comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, non, assumenda, dolor, corrupti dolore quibusdam velit sed voluptas quasi laboriosam voluptatem excepturi iusto accusantium ipsum autem impedit consequuntur voluptatibus eligendi!"
-  			},
-  			{
-  				name : "Task2",
-  				start: 23847238947,
-  				end: 2348723432, 
-  				comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, non, assumenda, dolor, corrupti dolore quibusdam velit sed voluptas quasi laboriosam voluptatem excepturi iusto accusantium ipsum autem impedit consequuntur voluptatibus eligendi!"
-  			}]
-  		}
-  	];
-
-	$.each(this.entries, function(i, value) {
-		var daySum = moment.duration(0);
-
-		$.each(value.tasks, function(i, task) {
-			task.duration = moment.duration(moment(task.end).diff(moment(task.start)));
-			daySum.add(task.duration);
-		});
-		value.duration = daySum;
-	});
+.service('historyService', function historyService($http, $q) {
 
   	this.getEntries = function() {
-  		return this.entries;
+      var entries = $q.defer();
+      $http.get('/bookings').success(function(data) {
+        var result =  {};
+        $.each(data, function(i, value) {
+
+          // convert string timestamps to numbers
+          if (value.data.start) value.data.start = parseInt(value.data.start);
+          if (value.data.end) value.data.end = parseInt(value.data.end);
+
+          // use start of day to group the bookings
+          var day = moment(value.data.start).startOf('day').valueOf();
+          if (!result[day]) {
+            result[day] = { "day" : day, "bookings" : [] };
+          }
+
+          // add a task description which is currently missing from the JSON data
+          value.data.name = "Dummy Task Description";
+
+          result[day].bookings.push(value.data);
+        });
+
+        // Convert result object to an array
+        var days = [];
+        $.each(result, function(i, value) {
+          days.push(value);
+        });
+
+        // calculate durations for each task and for the whole day
+        $.each(days, function(i, value) {
+          var daySum = moment.duration(0);
+
+          $.each(value.bookings, function(i, booking) {
+            if (booking.end) {
+              booking.duration = moment.duration(moment(booking.end).diff(moment(booking.start)));
+              daySum.add(booking.duration);
+            }
+          });
+          value.duration = daySum;
+        });
+
+        // resolve the promise and return the data
+        entries.resolve(days);
+      });
+
+  		return entries.promise;
   	}
 
   });
