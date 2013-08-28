9f2e69224527978356650096fdf52a3b00417621
Florian Maul <florian.maul@gmail.com>
2013-08-28 11:19:09 +0200

    Added services

---
+++ app/scripts/services/historyService.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fireTimeTracker')
+  .service('historyService', function historyService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ app/scripts/services/userService.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fireTimeTracker')
+  .service('userService', function userService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
