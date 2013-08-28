842065dbb42562dbf173669adb182754c5cc09ff
Florian Maul <florian.maul@gmail.com>
2013-08-28 10:49:05 +0200

    Added new views and controllers

---
+++ app/scripts/controllers/export.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fireTimeTracker')
+  .controller('ExportCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/time-track.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fireTimeTracker')
+  .controller('TimeTrackCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
