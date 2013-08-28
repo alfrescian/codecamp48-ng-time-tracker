c07b9337d4c10b1aa3f711c0284181a75aaf2ee6
Tom Beckmann <tomjonabc@gmail.com>
2013-08-28 02:45:36 +0200

    Added basic neo4j backend.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var neo4j = require('neo4j');
 
 var path = require('path');
+
+var User = require('./models/user.js');
+var Customer = require('./models/customer.js');
+var Project = require('./models/project.js');
+var Task = require('./models/task.js');
+var Booking = require('./models/booking.js');
+
+
+var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 // all environments
 app.use(express.logger('dev'));
@@ -25,17 +30,18 @@ if ('development' == app.get('env')) {
 }
 
 // middleware
-function auth(req, res) {
-	if (req.session.username == req.user && req.session.authorized) {
+function auth(req, res, next) {
+	if (!req.session.authorized || req.params.user != req.session.user) {
+		res.status(302).send("Not authorized");
+		return;
 	}
+	next();
 }
 
-io.sockets.on("connection", function(socket) {
+io.sockets.on('connection', function(socket) {
 });
 
-
 // routes
-/*
 app.get('/:user/:booking', auth, function(req, res) {
 });
 app.get('/:user/:task', auth, function(req, res) {
@@ -47,26 +53,40 @@ app.get('/:user/:customer', auth, function(req, res) {
 
 app.post('/login/:user', function(req, res) {
 });
-app.post('/:user/booking', auth, function(req, res) {
-	query -> name, datum start [end]
-	/tom/Name des booking?start=12345&end=23489
-});
-app.post('/:user/customer', auth, function(req, res) {
-	query -> name
-});
-app.post('/:user/:customer/project', auth, function(req, res) {
-	query -> name, estimatedTime
-});
-app.post('/:user/:project/task', auth, function(req, res) {
-	query -> name, estimatedTime
+
+app.post('/createUser', function(req, res) {
+	var node = db.createNode({name: req.body.name, username: req.body.username });
+	node.save(function (err, node) {
+		if (err) {
+			res.send(500);
+		} else {
+			res.send();
+		}
+	});
 });
 
-app.update('/:user/:booking', auth, function(req, res) {
-});*/
+app.post('/:user/booking', auth, function(req, res) {
+	// query -> name, datum start [end]
+	// /tom/Name%20des%20booking?start=12345&end=23489
+});
+app.post('/:user/customer', auth, function(req, res) {
+	// query -> name
+});
+app.post('/:user/:customer/project', auth, function(req, res) {
+	new Project({name: req.query.name, estimatedTime: req.query.estimatedTime}).create(function(err, node) {
+		console.log(node);
+	});
+	// query -> name, estimatedTime
+});
+app.post('/:user/:project/task', auth, function(req, res) {
+	// query -> name, estimatedTime
+});
+
+app.put('/:user/:booking', auth, function(req, res) {
+});
 
 // grunt specifics
 exports = module.exports = server;
 exports.use = function() {
 	app.use.apply(app, arguments);
 }
-
