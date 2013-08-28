18ab41febad33a66ca53e99654337350001a6733
Tom Beckmann <tomjonabc@gmail.com>
2013-08-28 10:22:40 +0200

    Fix crash on server start caused by route names

--- server/index.js
+++ server/index.js
@@ -6,13 +6,6 @@ var neo4j = require('neo4j');
 
 var path = require('path');
 
-var User = require('./models/user.js');
-var Customer = require('./models/customer.js');
-var Project = require('./models/project.js');
-var Task = require('./models/task.js');
-var Booking = require('./models/booking.js');
-
-
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 // all environments
@@ -22,17 +15,17 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('akj13i$q@2d'));
 app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static('../' + __dirname + '/app'));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 // middleware
 function auth(req, res, next) {
 	if (!req.session.authorized || req.params.user != req.session.user) {
-		res.status(302).send("Not authorized");
+		res.status(403).send("Not authorized");
 		return;
 	}
 	next();
@@ -42,16 +35,22 @@ io.sockets.on('connection', function(socket) {
 });
 
 // routes
-app.get('/:user/:booking', auth, function(req, res) {
+app.get('/user/:user/:booking', auth, function(req, res) {
+	res.send();
 });
-app.get('/:user/:task', auth, function(req, res) {
+app.get('/user/:user/:task', auth, function(req, res) {
+	res.send();
 });
-app.get('/:user/:project', auth, function(req, res) {
+app.get('/user/:user/:project', auth, function(req, res) {
+	res.send();
 });
-app.get('/:user/:customer', auth, function(req, res) {
+app.get('/user/:user/:customer', auth, function(req, res) {
+	res.send();
 });
 
 app.post('/login/:user', function(req, res) {
+	req.session.username = req.params.user;
+	req.session.userNode = findUser(req.session.username);
 });
 
 app.post('/createUser', function(req, res) {
@@ -65,24 +64,38 @@ app.post('/createUser', function(req, res) {
 	});
 });
 
-app.post('/:user/booking', auth, function(req, res) {
+app.post('/user/:user/booking', auth, function(req, res) {
 	// query -> name, datum start [end]
 	// /tom/Name%20des%20booking?start=12345&end=23489
 });
-app.post('/:user/customer', auth, function(req, res) {
+app.post('/user/:user/customer', auth, function(req, res) {
 	// query -> name
+	var node = db.createNode({name: req.body.name});
+	node.save(function(err, node) {
+		if (err) {
+			res.send(500);
+		} else {
+			node.createRelationshipTo(req.session.userNode, "WORKS_FOR", {}, function(err, rel) {
+				if (err) {
+					res.send(500);
+				} else {
+					res.send();
+				}
+			});
+		}
+	});
 });
-app.post('/:user/:customer/project', auth, function(req, res) {
-	new Project({name: req.query.name, estimatedTime: req.query.estimatedTime}).create(function(err, node) {
-		console.log(node);
+app.post('/user/:user/:customer/project', auth, function(req, res) {
+	var node = db.creatNode({name: req.body.name, estimatedTime: req.query.estimatedTime});
+	node.save(function(err, node) {
 	});
 	// query -> name, estimatedTime
 });
-app.post('/:user/:project/task', auth, function(req, res) {
+app.post('/user/:user/:project/task', auth, function(req, res) {
 	// query -> name, estimatedTime
 });
 
-app.put('/:user/:booking', auth, function(req, res) {
+app.put('/update/:user/:booking', auth, function(req, res) {
 });
 
 // grunt specifics
@@ -90,3 +103,18 @@ exports = module.exports = server;
 exports.use = function() {
 	app.use.apply(app, arguments);
 }
+
+function findUser(username, callback) {
+	var userNode = db.query([
+		'START n=node(*)',
+		'WHERE has(n.username) and (n.username="' + username + '")',
+		'RETURN n'
+	].join('\n'), function(err, res) {
+		if (err || !res || res.length == 0) {
+			callback(err, null);
+		} else {
+			callback(err, res[0]);
+		}
+	});
+}
+
